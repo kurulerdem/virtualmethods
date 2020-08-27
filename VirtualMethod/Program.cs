6092881d8acacfe292004000a299769a95bcc84e
kurulerdem <kurulerdem1@gmail.com>
2020-08-27 22:53:20 +0300

    Virtual Methods

---
+++ VirtualMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VirtualMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tesla tesla = new Tesla();
+            tesla.Drive();
+        }
+    }
+
+    class Car
+    {
+        public virtual void Drive()
+        {
+            Console.WriteLine("Driving Manuel");
+        }
+    }
+    class Tesla : Car
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("Driving Auto");
+        }
+    }
+}
